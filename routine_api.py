683dcab3223624435324966328387e8df0b1e24d
Ekum <ekum.jaswal@gmail.com>
2025-04-14 17:27:27 +0100

    setting up API calls

--- routine_api.py
+++ routine_api.py
@@ -1,1 +1,18 @@
-# Interacts with the API.
+import requests
+
+ENDPOINT = "http://makeup-api.herokuapp.com/api/v1/products.json"
+
+def get_all_products():
+    response = requests.get(url=ENDPOINT)
+    response.raise_for_status()
+    data = response.json()
+    return data
+
+def get_products_by_brand(brand):
+    brand_params = {"brand": brand}
+    response = requests.get(url=ENDPOINT, params=brand_params)
+    response.raise_for_status()
+    data = response.json()
+    return data
+
+
