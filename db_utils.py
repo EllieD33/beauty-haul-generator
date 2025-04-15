a795d26f5806d9bb0606a4a7d3669ac900431671
Laura Wheaton <laura-wheaton@hotmail.co.uk>
2025-04-15 09:28:43 +0100

    Written out a function to append the routine to a CSV file

--- db_utils.py
+++ db_utils.py
@@ -1,1 +1,12 @@
 # File containing utility functions for accessing database.
+import csv
+from fileinput import close
+
+
+def save_routine(routine, filename="data/user_routines.csv"):
+    headers = ["Category", "Name", "Price"]
+    with open(filename, "a") as csv_file:
+        writer=csv.DictWriter(csv_file, fieldnames=headers)
+        writer.writeheader()
+        writer.writerows(routine)
+    print("Your routine has been saved successfully!💾")
