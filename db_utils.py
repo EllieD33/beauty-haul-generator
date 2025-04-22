d06d218395674503b905c9779cca84807c6bd15a
Laura Wheaton <laura-wheaton@hotmail.co.uk>
2025-04-22 15:29:14 +0100

    Added in method to convert the output to a dictionary, and then used this to save the routine as a dictionary in the db_utils save routine function.

--- db_utils.py
+++ db_utils.py
@@ -5,6 +5,13 @@ import os.path
 
 def save_routine(routine, filename="data/user_routines.csv"):
     headers = ["Brand", "Product", "Price", "Description"]
+    # create a list of dictionaries for the routine
+    routine_dict = []
+    for product, score in routine:
+        product_dict = product.routine_to_dict() # convert the products to a dictionary
+        product_values = list(product_dict.values())
+        each_row = dict(zip(headers,product_values)) # combine the headers with the values in each row
+        routine_dict.append(each_row)
     #check if file exists
     file_exists = os.path.exists(filename)
 
